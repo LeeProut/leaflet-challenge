daca994bdb6c4a74e4835dd1cd712b9859d863fc
Lee Prout <wleeprout@gmail.com>
2021-01-23 21:16:35 -0500

    all layers working, orange cast fixed with fillOpacity 0

--- Leaflet-Step-2/static/js/map2.js
+++ Leaflet-Step-2/static/js/map2.js
@@ -2,16 +2,9 @@
 // this data is updated every minute
 var geoJson = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
+// techtonic plate geoJson data
 var techPlates = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
 
-// Create a new map, centering over Topeka, Kansas to show the entire U.S.
-// var myMap = L.map("map", {
-//   center: [
-//     39.06263402134599, -95.72554190670454
-//   ],
-//   zoom: 3,
-//   layers: [lightMap, satMap]
-// });
 
 // define map layers
 var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -32,35 +25,47 @@ var satMap =  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
       accessToken: API_key
     }); 
 
+var outdoorsMap =  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/outdoors-v9",
+      accessToken: API_key
+    });     
+
 // Create a new map, centering over Topeka, Kansas to show the entire U.S.
 var myMap = L.map("map", {
   center: [
     39.06263402134599, -95.72554190670454
   ],
-  zoom: 3,
-  layers: [lightMap, satMap]
+  zoom: 3.5,
+  layers: [lightMap]
 });    
     
 // Define a baseMaps object to hold base layers
 var baseMaps = {
     "Light Map": lightMap,
-    "Sat Map": satMap
+    "Sat Map": satMap,
+    "Outdoors Map": outdoorsMap,
 };
 
+// define variables for layer groups for earthquakes and techtonic plates
 var quakesLayer = L.layerGroup();
 var platesLayer = L.layerGroup();
 
+// Define an overlayMaps object to hold layers for earthquakes and techtonic plates
 var overlayMaps = {
   Earthquakes: quakesLayer,
   Plates: platesLayer
 }
 
-// add control for layers
+// add control for layers that is always visible (no collapse)
 L.control.layers(baseMaps, overlayMaps, {
-    // collapsed: false
+    collapsed: false,
 }).addTo(myMap);
 
-
+// d3 for fetching earthquake geoJson data
     d3.json(geoJson).then(data => {
         console.log(data);
         var earthquakes = data.features;
@@ -83,11 +88,11 @@ L.control.layers(baseMaps, overlayMaps, {
             return "#F4A460";        
         }
       }; 
-        
+      // functions to multiply the magnitude to render circles larger for visibility  
       function markerSize(mag) {
         return mag * 4;
       };
-    
+        // geoJson earthquake data for rendering circles according to earthquake magnitude
         L.geoJSON(data, {
           pointToLayer: function (feature, latlng) {        
             return L.circleMarker(latlng, {
@@ -98,7 +103,7 @@ L.control.layers(baseMaps, overlayMaps, {
             });
           },
           
-          //onEachFeature: onEachEarthquake 
+          //onEachFeature for popup with data from geoJson features 
           onEachFeature: function(feature, layer) {
             layer.bindPopup(`
             <h3>${feature.properties.place}</h3>
@@ -137,12 +142,15 @@ L.control.layers(baseMaps, overlayMaps, {
 // bring in techtonic plate data   
   d3.json(techPlates).then(plateData => {
     console.log(plateData);
-    var plates = plateData.features;
-    console.log(plates);
-
-
-  //add to platesLayer
-  //add to myMap    
+    // geoJSON techtonic plate data for rendering faultlines
+    // using fillOpacity: 0 fixed the orange cast over map
+    L.geoJSON(plateData.features, {
+      style: {
+        color: "orange",
+        fillOpacity: 0,
+      }
+    }).addTo(platesLayer);
+    platesLayer.addTo(myMap);   
 
 // closing D3    
   });
